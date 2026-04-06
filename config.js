ab398b616baa9bd67ace205c935dddecc6a22b5c
DesarrolloCombuses <desarrollotecnologico@combuses.com.co>
2026-04-06 18:33:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 window.SUPABASE_CONFIG = {
-  url: "https://TU-PROYECTO.supabase.co",
-  anonKey: "TU_SUPABASE_ANON_KEY",
-  conductoresCsvUrl: "https://docs.google.com/spreadsheets/d/e/TU_CSV/pub?gid=0&single=true&output=csv",
-  sicovDispatchUrl: "https://TU-PROYECTO.supabase.co/functions/v1/sicov-alistamiento",
-  sicovDispatchKey: "TU_SUPABASE_ANON_KEY",
+  url: "https://cbplebkmxrkaafqdhiyi.supabase.co",
+  anonKey: "sb_publishable_DZCceNTENY4ViP17-eZrGg_bdMElZ9X",
+  conductoresCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vThNrFZLbNklMFtPeg0wF4TA1vZHnZ4YNMmGcnHfty_RoNuAQw__iV2GMXqTsv36MPiks1ARpYui1JK/pub?gid=0&single=true&output=csv",
+  sicovDispatchUrl: "https://cbplebkmxrkaafqdhiyi.supabase.co/functions/v1/sicov-alistamiento",
+  sicovDispatchKey: "sb_publishable_DZCceNTENY4ViP17-eZrGg_bdMElZ9X",
   sicovTimeoutMs: 25000
 };
+
